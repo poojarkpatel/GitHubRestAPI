6232023b79e4e229ac8be1f6c0bc975ac017d774
Pooja Patel <poojarkpatel0397@gmail.com>
2020-09-30 17:52:03 -0400

    Final Commit

--- Rest_API.py
+++ Rest_API.py
@@ -1,24 +1,36 @@
+"""
+Created on September 30 2020
+
+@author: Pooja Patel
+cwid: 10456923
+
+This file is a test for the Rest_API file.
+
+"""
+
 import json
 import requests
 
 
 def repo_list(user_id):
+    """ generator functions that returns repo name with number of commits in each repo """
     repo_url = requests.get("https://api.github.com/users/{}/repos".format(user_id))
     repo_list_name = repo_url.json()
     for value in repo_list_name:
         commit_count = 0
         repo_commits = requests.get("https://api.github.com/repos/{}/{}/commits".format(user_id, value["name"]))
-        for commit in repo_commits.json():
-            if commit in repo_commits.json():
+        commits = repo_commits.json()
+        for commit in commits:
+            if commit in commits:
                 commit_count += 1
-        yield "Repo: {}  and Number of commits: {} ".format(value["name"], commit_count)
+        yield "Repo: {}  and Number of commits: {}".format(value["name"], commit_count)
 
 
 def get_user_id():
+    """ get the git user ID"""
     user_id = input("Enter your user id: ")
+    repo_list(user_id)
     for item in repo_list(user_id):
         print(item)
 
-
-if __name__ == '__main__':
-    get_user_id()
+get_user_id()
--- Rest_API_test.py
+++ Rest_API_test.py
@@ -1,30 +1,30 @@
 """
-Created on February 18 2020
+Created on September 30 2020
 
-@author:
-cwid:
+@author: Pooja Patel
+cwid: 10456923
 
-This file is a test for the Gitrepo file.
+This file is a test for the Rest_API file.
 
 """
 import unittest
-from Rest_API import repo_list, get_user_id
+from Rest_API import repo_list
 
 
 class TestRepo(unittest.TestCase):
-    """Test for the repositoryList function"""
+    """Test for the repo_list function"""
     def test_repo_list(self):
         repo = [value for value in repo_list("poojarkpatel")]
         result = ['Repo: Banking-System  and Number of commits: 13',
-                  'Repo: GitHubRestAPI  and Number of commits: 1 ',
-                  'Repo: helloworld  and Number of commits: 6 ',
-                  'Repo: Sentiment_Analyzer  and Number of commits: 2 ',
-                  'Repo: Smart_IDcard  and Number of commits: 2 ',
+                  'Repo: GitHubRestAPI  and Number of commits: 1',
+                  'Repo: helloworld  and Number of commits: 6',
+                  'Repo: Sentiment_Analyzer  and Number of commits: 2',
+                  'Repo: Smart_IDcard  and Number of commits: 2',
                   'Repo: Smart_Id_Card  and Number of commits: 2',
-                  'Repo: SSW555_GEDCOM_Analyzer  and Number of commits: 4 ',
+                  'Repo: SSW555_GEDCOM_Analyzer  and Number of commits: 4',
                   'Repo: Student_Repository  and Number of commits: 30',
-                  'Repo: Testing_Triangle_Classification  and Number of commits: 2 ',
-                  "Repo: Triangle567  and Number of commits: 8 "
+                  'Repo: Testing_Triangle_Classification  and Number of commits: 2',
+                  "Repo: Triangle567  and Number of commits: 8"
                   ]
         self.assertEqual(repo, result)
 
