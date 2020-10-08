6540b41db6667fc5707b85edcc4da3ff8a6b1ae9
Pooja Patel <poojarkpatel0397@gmail.com>
2020-10-08 15:05:52 -0400

    mock

--- Rest_API.py
+++ Rest_API.py
@@ -1,35 +1,25 @@
 """
-Created on September 30 2020
-
 @author: Pooja Patel
 cwid: 10456923
-
 This file is a test for the Rest_API file.
 
 """
-
-import json
 import requests
 
 
 def repo_list(user_id):
     """ generator functions that returns repo name with number of commits in each repo """
     repo_url = requests.get("https://api.github.com/users/{}/repos".format(user_id))
-    repo_list_name = repo_url.json()
-    for value in repo_list_name:
-        commit_count = 0
-        repo_commits = requests.get("https://api.github.com/repos/{}/{}/commits".format(user_id, value["name"]))
-        commits = repo_commits.json()
-        for commit in commits:
-            if commit in commits:
-                commit_count += 1
-        yield "Repo: {} and Number of commits: {}".format(value["name"], commit_count)
+    list_repo = [item["name"] for item in repo_url.json()]
+    return list_repo
 
 
-# def get_user_id():
-#     """ get the git user ID"""
-#     user_id = input("Enter your user id: ")
-#     for item in repo_list(user_id):
-#         print(item)
+def commits(user_id, repo):
+    repo_commit = requests.get("https://api.github.com/repos/{}/{}/commits".format(user_id, repo))
+    commit_count = 0
+    for _ in repo_commit.json():
+        commit_count += 1
+    return commit_count
 
-# get_user_id()
+
+
--- Rest_API_test.py
+++ Rest_API_test.py
@@ -1,31 +1,70 @@
+
 """
 Created on September 30 2020
-
 @author: Pooja Patel
 cwid: 10456923
-
-This file is a test for the Rest_API file.
-
 """
-import unittest
-from unittest import mock
-from Rest_API import repo_list
 
-@mock.patch('urllib.request.urlopen')
-class TestRepo(unittest.TestCase):
-    """Test for the repo_list function"""
-    def test_repo_list(self, other):
-        repo = [value for value in repo_list("richkempinski")]
-        result = ['Repo: csp and Number of commits: 2',
-                  'Repo: hellogitworld and Number of commits: 30',
-                  'Repo: helloworld and Number of commits: 6',
-                  'Repo: Mocks and Number of commits: 10',
-                  'Repo: Project1 and Number of commits: 2',
-                  'Repo: richkempinski.github.io and Number of commits: 9',
-                  'Repo: threads-of-life and Number of commits: 1',
-                  'Repo: try_nbdev and Number of commits: 2',
-                  'Repo: try_nbdev2 and Number of commits: 5']
-        self.assertEqual(repo, result)
+import unittest
+import json
+import requests
+from Rest_API import repo_list
+from Rest_API import commits
+from unittest import mock
+
+
+class FakeResponse:
+    def __init__(self, json_data):
+        self.json_data = json_data
+
+    def json(self):
+        return self.json_data
+
+switcher = {"https://api.github.com/users/richkempinski/repos": "Repository.json",
+            "https://api.github.com/repos/richkempinski/csp/commits": "csp.json",
+            "https://api.github.com/repos/richkempinski/hellogitworld/commits": "hellogitworld.json",
+            "https://api.github.com/repos/richkempinski/helloworld/commits": "helloworld.json",
+            "https://api.github.com/repos/richkempinski/Mocks/commits": "Mocks.json",
+            "https://api.github.com/repos/richkempinski/Project1/commits": "Project1.json",
+            "https://api.github.com/repos/richkempinski/richkempinski.github.io/commits":
+                "richkempinski.github.io.json",
+            "https://api.github.com/repos/richkempinski/try_nbdev/commits": "try_nbdev.json",
+            "https://api.github.com/repos/richkempinski/try_nbdev2/commits": "try_nbdev2.json",
+            "https://api.github.com/repos/richkempinski/threads-of-life/commits": "threads-of-life.json"}
+
+
+def mocked_request_commit(*args):
+    if args[0] in switcher:
+        with open(switcher[args[0]]) as f:
+            return FakeResponse(json.load(f))
+    return FakeResponse(None)
+
+
+def mocked_request_get(*args):
+    if args[0] in switcher:
+        with open(switcher[args[0]]) as f:
+            return FakeResponse(json.load(f))
+    return FakeResponse(None)
+
+
+class TestRequestGitRepos(unittest.TestCase):
+    @mock.patch('requests.get', side_effect=mocked_request_get)
+    def test_repo(self, mock_get):
+        expected = ['csp', 'hellogitworld', 'helloworld', 'Mocks', 'Project1',
+                            'richkempinski.github.io', 'threads-of-life', 'try_nbdev', 'try_nbdev2']
+        self.assertEqual(repo_list("richkempinski"), expected)
+
+    @mock.patch('requests.get', side_effect=mocked_request_commit)
+    def test_commits(self, mock_get):
+        self.assertEqual(commits("richkempinski", "csp"), 2)
+        self.assertEqual(commits("richkempinski", 'hellogitworld'), 30)
+        self.assertEqual(commits("richkempinski", 'helloworld'), 6)
+        self.assertEqual(commits("richkempinski", 'Mocks'), 10)
+        self.assertEqual(commits("richkempinski", 'Project1'), 2)
+        self.assertEqual(commits("richkempinski", 'richkempinski.github.io'), 9)
+        self.assertEqual(commits("richkempinski", 'threads-of-life'), 1)
+        self.assertEqual(commits("richkempinski", 'try_nbdev'), 2)
+        self.assertEqual(commits("richkempinski", 'try_nbdev2'), 5)
 
 
 if __name__ == '__main__':
